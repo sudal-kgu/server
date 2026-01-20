5aab425893c061e141f7bf7532eb153e7f449af2
한권희 <jana32496@gmail.com>
2026-01-20 21:45:59 +0900

    chore: Swagger UI(OpenAPI) 설정 추가
    - 배포 환경(Nginx/HTTPS) 이슈 방지를 위해 Server URL을 '/'로 설정

---
+++ src/main/java/store/sonyk9919/api/global/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package store.sonyk9919.api.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI springBootAPI() {
+
+        Info info = new Info()
+                .title("untitled (KGU) API Documentation") // 팀명 확정 시 변경
+                .description("심화 캡스톤: AI 분리수거 사전 서버 API 문서입니다.")
+                .contact(new io.swagger.v3.oas.models.info.Contact()
+                        .name("Server Repository")
+                        .url("https://github.com/untitled-KGU/server"))
+                .version("1.0.0");
+
+
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .info(info);
+    }
+
+// TODO: 추후 API가 많아지면 아래 예시처럼 도메인별로 그룹핑하기
+//    @Bean
+//    public GroupedOpenApi trashGroup() {
+//        return GroupedOpenApi.builder()
+//                .group("1. 쓰레기 분석")
+//                .pathsToMatch("/api/v1/trash/**")
+//                .build();
+//    }
+}
