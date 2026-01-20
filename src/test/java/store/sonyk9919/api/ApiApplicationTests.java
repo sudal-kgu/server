f701ae49413a676069346210643d79148d87b491
한권희 <jana32496@gmail.com>
2026-01-20 21:37:13 +0900

    chore: 스프링 부트 프로젝트 초기 설정
    - JPA, Lombok, Validation, H2, MySQL, Swagger 필수 의존성 추가
    - .gitignore 설정 추가 (DB 파일, IDE 설정, Secret 파일 제외)

---
+++ src/test/java/store/sonyk9919/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package store.sonyk9919.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
