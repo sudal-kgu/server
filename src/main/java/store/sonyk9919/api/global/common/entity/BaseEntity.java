d47f59b48dfba4e6bddc6a5b99ecc07a85e22c3f
한권희 <jana32496@gmail.com>
2026-01-21 11:21:00 +0900

    feat: JPA Auditing 적용 및 BaseEntity 공통 클래스 구현
    - 엔티티 생성일(createdAt) 자동 관리로 BaseEntity 추상 클래스 작성

---
+++ src/main/java/store/sonyk9919/api/global/common/entity/BaseEntity.java
@@ -0,0 +1,19 @@
+package store.sonyk9919.api.global.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+}
