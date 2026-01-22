6ea71dcc67d310628772087eb348871bb8866658
한권희 <jana32496@gmail.com>
2026-01-22 23:08:24 +0900

    refactor: 에러 응답 코드 리뷰 반영
    - 5xx 에러 공통 코드로 통일 및 4xx 에러 세분화 진행

--- src/main/java/store/sonyk9919/api/global/common/dto/ErrorStatus.java
+++ src/main/java/store/sonyk9919/api/global/common/dto/ErrorStatus.java
@@ -8,15 +8,13 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorStatus implements BaseResponseStatus {
 
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, "BAD_REQUEST", "잘못된 요청입니다."),
-    VALIDATION_FAIL(HttpStatus.BAD_REQUEST, "VALIDATION_FAIL", "입력값이 올바르지 않습니다."),
-    INVALID_REQUEST_BODY(HttpStatus.BAD_REQUEST, "INVALID_REQUEST_BODY", "요청 본문 형식이 올바르지 않습니다."),
-    NO_PERMISSION(HttpStatus.FORBIDDEN, "NO_PERMISSION", "해당 리소스에 접근할 권한이 없습니다."),
-    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED,"METHOD_NOT_ALLOWED","지원하지 않는 HTTP 메소드 입니다."),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON_400", "잘못된 요청입니다."),
+    INVALID_REQUEST_BODY(HttpStatus.BAD_REQUEST, "COMMON_400_1", "요청 본문 형식이 올바르지 않습니다."),
+    VALIDATION_FAIL(HttpStatus.BAD_REQUEST, "COMMON_400_2", "입력값이 올바르지 않습니다."),
+    NO_PERMISSION(HttpStatus.FORBIDDEN, "COMMON_403", "접근 권한이 없습니다."),
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED,"COMMON_405","지원하지 않는 HTTP 메소드 입니다."),
 
-    DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DATABASE_ERROR", "데이터베이스 오류가 발생했습니다."),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR", "서버 내부 오류가 발생했습니다.");
-
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_500", "서버 내부 오류가 발생했습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
