91549d34ac3f469edbcc1846decc4e7ab0d6c05e
sonyk9919 <sonyk9919@sonyk9919ui-MacBookAir.local>
2026-01-27 23:16:52 +0900

    test: Taxonomy 테이블 데이터 및 조회 기능 검증 테스트 작성
    - 전체 데이터 개수 일치 여부
    - Category와 SubCategory 쌍의 유니크 제약조건 확인
    - Category의 name과 SubCategory의 alias를 통한 단건 조회
    - 현재 N + 1 문제를 해결하기 위해 간단히 @EntitiyGraph를 사용했지만 추후 JPQL, QueryDSL을 통해 최적화 필요

--- src/main/java/store/sonyk9919/api/domain/taxonomy/repository/TrashTaxonomyRepository.java
+++ src/main/java/store/sonyk9919/api/domain/taxonomy/repository/TrashTaxonomyRepository.java
@@ -1,7 +1,19 @@
 package store.sonyk9919.api.domain.taxonomy.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.Repository;
 import store.sonyk9919.api.domain.taxonomy.entitiy.TrashTaxonomy;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface TrashTaxonomyRepository extends Repository<TrashTaxonomy, Long> {
+
+    int count();
+
+    @EntityGraph(attributePaths = { "category", "subCategory" })
+    List<TrashTaxonomy> findAll();
+
+    @EntityGraph(attributePaths = { "category", "subCategory" })
+    Optional<TrashTaxonomy> getTrashTaxonomiesByCategory_NameAndSubCategory_Alias(String name, String alias);
 }
