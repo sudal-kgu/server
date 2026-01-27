a62276a4511994ccfa89ba33c61938570ad7b306
sonyk9919 <sonyk9919@sonyk9919ui-MacBookAir.local>
2026-01-27 23:45:12 +0900

    test: SubCategory의 전체 데이터 개수 검증

---
+++ src/test/java/store/sonyk9919/api/domain/taxonomy/repository/TrashSubCategoryRepositoryTest.java
@@ -0,0 +1,20 @@
+package store.sonyk9919.api.domain.taxonomy.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+@SpringBootTest
+class TrashSubCategoryRepositoryTest {
+
+    @Autowired
+    private TrashSubCategoryRepository trashSubCategoryRepository;
+
+    @Test
+    @DisplayName("전체 SubCategory 287개")
+    void isCountCorrect() { assertThat(trashSubCategoryRepository.count()).isEqualTo(287); }
+}
