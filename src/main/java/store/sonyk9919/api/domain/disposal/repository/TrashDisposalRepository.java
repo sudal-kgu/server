becbd6eb730023a5f9e96908df2bf83c67ec0ed8
sonyk9919 <sonyk9919@sonyk9919ui-MacBookAir.local>
2026-02-18 16:28:26 +0900

    test: Disposal 데이터 정합성
    - 전체 데이터가 94개 임을 확인

--- src/main/java/store/sonyk9919/api/domain/disposal/repository/TrashDisposalRepository.java
+++ src/main/java/store/sonyk9919/api/domain/disposal/repository/TrashDisposalRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.repository.Repository;
 import store.sonyk9919.api.domain.disposal.entity.TrashDisposal;
 
 public interface TrashDisposalRepository extends Repository<TrashDisposal, Long> {
+
+    int count();
 }
