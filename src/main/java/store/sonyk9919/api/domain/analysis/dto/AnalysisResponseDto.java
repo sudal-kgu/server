eb1902caa54fbc6aa576a03ce18cf37c36143d02
한권희 <jana32496@gmail.com>
2026-02-21 22:26:53 +0900

    refactor: 분석 서버의 맞춰 dto 필드값 수정
    - 이전 label,material에서 category,subcategory로 필드 변경
    - 누락된 access값 설정

--- src/main/java/store/sonyk9919/api/domain/analysis/dto/AnalysisResponseDto.java
+++ src/main/java/store/sonyk9919/api/domain/analysis/dto/AnalysisResponseDto.java
@@ -2,12 +2,13 @@ package store.sonyk9919.api.domain.analysis.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString
 public class AnalysisResponseDto {
 
@@ -20,16 +21,16 @@ public class AnalysisResponseDto {
     private List<DetectedItem> detectedItems;
 
     @Getter
-    @NoArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
     @ToString
     public static class DetectedItem {
-        private String label;
-        private String material;
+        private String category;
+        private String subcategory;
         private Confidence confidence;
         private String filename;
 
         @Getter
-        @NoArgsConstructor
+        @NoArgsConstructor(access = AccessLevel.PROTECTED)
         @ToString
         public static class Confidence {
             private Double object;
