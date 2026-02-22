fc77e3d3604e7954b83cce526ce3a17b1bbdb7c1
한권희 <jana32496@gmail.com>
2026-02-22 14:05:03 +0900

    test: Trash 및 TrashTaxonomy 레포지토리 커스텀 조회 테스트
    - TrashTaxonomyRepository: 조합 키(name:alias)로 다중 조회 테스트 작성
    - TrashRepository: request_id로 분석된 쓰레기 데이터 조회 테스트 작성

---
+++ src/test/java/store/sonyk9919/api/domain/trash/repository/TrashRepositoryTest.java
@@ -0,0 +1,50 @@
+package store.sonyk9919.api.domain.trash.repository;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import store.sonyk9919.api.domain.analysis.entity.AnalysisRequest;
+import store.sonyk9919.api.domain.analysis.repository.AnalysisRequestRepository;
+import store.sonyk9919.api.domain.taxonomy.entitiy.TrashTaxonomy;
+import store.sonyk9919.api.domain.taxonomy.repository.TrashTaxonomyRepository;
+import store.sonyk9919.api.domain.trash.entity.Trash;
+
+@SpringBootTest
+@Transactional
+class TrashRepositoryTest {
+    @Autowired
+    private TrashRepository trashRepository;
+
+    @Autowired
+    private AnalysisRequestRepository analysisRequestRepository;
+
+    @Autowired
+    private TrashTaxonomyRepository trashTaxonomyRepository;
+
+    @Test
+    @DisplayName("request_id로 분석된 쓰레기 데이터들을 조회")
+    void findByAnalysisRequest_RequestId_SimpleTest() {
+        AnalysisRequest request = AnalysisRequest.createWithUUID();
+        analysisRequestRepository.save(request);
+
+        TrashTaxonomy taxonomy = trashTaxonomyRepository.findAll().get(0);
+
+        Trash trash1 = Trash.create(request, taxonomy, "crop01.png");
+        Trash trash2 = Trash.create(request, taxonomy, "crop02.png");
+        trashRepository.save(trash1);
+        trashRepository.save(trash2);
+
+        String requestId = request.getRequestId();
+        List<Trash> result = trashRepository.findByAnalysisRequest_RequestId(requestId);
+
+        assertThat(result).hasSize(2);
+        assertThat(result).extracting(Trash::getFilename)
+                .containsExactlyInAnyOrder("crop01.png", "crop02.png");
+    }
+}
