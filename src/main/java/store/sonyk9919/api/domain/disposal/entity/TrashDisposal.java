815bc379f5a552361bf960856993dfa5731840f0
sonyk9919 <sonyk9919@sonyk9919ui-MacBookAir.local>
2026-02-26 15:54:15 +0900

    refact: 다국어 지원을 위해 처리방법 엔티티 변경

--- src/main/java/store/sonyk9919/api/domain/disposal/entity/TrashDisposal.java
+++ src/main/java/store/sonyk9919/api/domain/disposal/entity/TrashDisposal.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import store.sonyk9919.api.global.language.type.Language;
 
 @Entity
 @Getter
@@ -16,5 +17,19 @@ public class TrashDisposal {
     private Long id;
 
     @Column(nullable = false, columnDefinition = "TEXT")
-    private String guide;
+    private String ko;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String en;
+
+    public String getGuide() {
+        return getGuide(Language.KO);
+    }
+
+    public String getGuide(Language language) {
+        return switch (language) {
+            case KO -> ko;
+            case EN -> en;
+        };
+    }
 }
