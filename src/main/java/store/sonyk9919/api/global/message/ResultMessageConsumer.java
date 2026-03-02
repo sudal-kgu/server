e80e984f63206ab2301d77c9d2ccf5ec454e471b
한권희 <jana32496@gmail.com>
2026-03-02 14:07:43 +0900

    feat: RabbitMQ producer, consumer 추가
    - producer는 request 큐로 request_id를 넣기
    - consumer는 result 큐를 리스닝하며 분석 결과를 받아온다.

---
+++ src/main/java/store/sonyk9919/api/global/message/ResultMessageConsumer.java
@@ -0,0 +1,33 @@
+package store.sonyk9919.api.global.message;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+import store.sonyk9919.api.domain.analysis.dto.AnalysisResponseDto;
+import store.sonyk9919.api.domain.analysis.service.AnalysisResultNotifier;
+import store.sonyk9919.api.global.common.dto.ErrorStatus;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ResultMessageConsumer {
+    private final AnalysisResultNotifier analysisResultNotifier;
+
+    @RabbitListener(queues = "${rabbitmq.result-queue-name}")
+    public void consume(AnalysisResponseDto analysisResponseDto){
+        log.info("[MQ] Received result: requestId={}", analysisResponseDto.getRequestId());
+        try {
+            analysisResultNotifier.saveAndNotify(
+                    analysisResponseDto.getRequestId(),
+                    analysisResponseDto
+            );
+        } catch (Exception e) {
+            log.error("[MQ] Failed to process result: requestId={}", analysisResponseDto.getRequestId(), e);
+            analysisResultNotifier.notifyError(
+                    analysisResponseDto.getRequestId(),
+                    ErrorStatus.INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+}
