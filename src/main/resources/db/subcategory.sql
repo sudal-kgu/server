ada7db6485db762e75c1a3a28de7bfd8aab4d202
sonyk9919 <sonyk9919@sonyk9919ui-MacBookAir.local>
2026-03-11 16:59:24 +0900

    chore: db/**.sql 내용을 전부 소문자로 통일

--- src/main/resources/db/subcategory.sql
+++ src/main/resources/db/subcategory.sql
@@ -1,77 +1,77 @@
 -- 가구류
-INSERT INTO trash_sub_category (ko, en) VALUES ('공통(가구류)', 'Common(Furniture)');
+insert into trash_sub_category (ko, en) values ('공통(가구류)', 'Common(Furniture)');
 
 -- 고철류
-INSERT INTO trash_sub_category (ko, en) VALUES ('고철(고철류)', 'Scrap Metal(Scrap Metal)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('비철금속(고철류)', 'Non-ferrous Metal(Scrap Metal)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('주전자(고철류)', 'Kettle(Scrap Metal)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('프라이팬(고철류)', 'Frying Pan(Scrap Metal)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('두발자전거(자전거)', 'Bicycle(Bicycle)');
+insert into trash_sub_category (ko, en) values ('고철(고철류)', 'Scrap Metal(Scrap Metal)');
+insert into trash_sub_category (ko, en) values ('비철금속(고철류)', 'Non-ferrous Metal(Scrap Metal)');
+insert into trash_sub_category (ko, en) values ('주전자(고철류)', 'Kettle(Scrap Metal)');
+insert into trash_sub_category (ko, en) values ('프라이팬(고철류)', 'Frying Pan(Scrap Metal)');
+insert into trash_sub_category (ko, en) values ('두발자전거(자전거)', 'Bicycle(Bicycle)');
 
 -- 불연성 종량제
-INSERT INTO trash_sub_category (ko, en) VALUES ('그릇류(도기류)', 'Dishes(Ceramics)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('뚝배기(도기류)', 'Earthenware Pot(Ceramics)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('병(도기류)', 'Bottle(Ceramics)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('컵(도기류)', 'Cup(Ceramics)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('항아리(도기류)', 'Jar(Ceramics)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('화분(도기류)', 'Flower Pot(Ceramics)');
+insert into trash_sub_category (ko, en) values ('그릇류(도기류)', 'Dishes(Ceramics)');
+insert into trash_sub_category (ko, en) values ('뚝배기(도기류)', 'Earthenware Pot(Ceramics)');
+insert into trash_sub_category (ko, en) values ('병(도기류)', 'Bottle(Ceramics)');
+insert into trash_sub_category (ko, en) values ('컵(도기류)', 'Cup(Ceramics)');
+insert into trash_sub_category (ko, en) values ('항아리(도기류)', 'Jar(Ceramics)');
+insert into trash_sub_category (ko, en) values ('화분(도기류)', 'Flower Pot(Ceramics)');
 
 -- 비닐류
-INSERT INTO trash_sub_category (ko, en) VALUES ('식품봉지(비닐)', 'Food Bag(Vinyl)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('리필용기(비닐)', 'Refill Container(Vinyl)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('봉투(비닐)', 'Plastic Bag(Vinyl)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('에어캡(비닐)', 'Bubble Wrap(Vinyl)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('포장재(비닐)', 'Packaging(Vinyl)');
+insert into trash_sub_category (ko, en) values ('식품봉지(비닐)', 'Food Bag(Vinyl)');
+insert into trash_sub_category (ko, en) values ('리필용기(비닐)', 'Refill Container(Vinyl)');
+insert into trash_sub_category (ko, en) values ('봉투(비닐)', 'Plastic Bag(Vinyl)');
+insert into trash_sub_category (ko, en) values ('에어캡(비닐)', 'Bubble Wrap(Vinyl)');
+insert into trash_sub_category (ko, en) values ('포장재(비닐)', 'Packaging(Vinyl)');
 
 -- 스티로폼류
-INSERT INTO trash_sub_category (ko, en) VALUES ('네모트레이(스티로폼류)', 'Square Tray(Styrofoam)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('보호재(스티로폼류)', 'Buffer(Styrofoam)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('일반스티로폼(스티로폼류)', 'General Styrofoam(Styrofoam)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('포장용기(스티로폼류)', 'Packaging Container(Styrofoam)');
+insert into trash_sub_category (ko, en) values ('네모트레이(스티로폼류)', 'Square Tray(Styrofoam)');
+insert into trash_sub_category (ko, en) values ('보호재(스티로폼류)', 'Buffer(Styrofoam)');
+insert into trash_sub_category (ko, en) values ('일반스티로폼(스티로폼류)', 'General Styrofoam(Styrofoam)');
+insert into trash_sub_category (ko, en) values ('포장용기(스티로폼류)', 'Packaging Container(Styrofoam)');
 
 -- 유리병류
-INSERT INTO trash_sub_category (ko, en) VALUES ('기타술병(유리병)', 'Other Liquor Bottles(Glass Bottle)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('맥주병(유리병)', 'Beer Bottle(Glass Bottle)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('박카스병(유리병)', 'Bacchus Bottle(Glass Bottle)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('소주병(유리병)', 'Soju Bottle(Glass Bottle)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('음료병(유리병)', 'Drink Bottle(Glass Bottle)');
+insert into trash_sub_category (ko, en) values ('기타술병(유리병)', 'Other Liquor Bottles(Glass Bottle)');
+insert into trash_sub_category (ko, en) values ('맥주병(유리병)', 'Beer Bottle(Glass Bottle)');
+insert into trash_sub_category (ko, en) values ('박카스병(유리병)', 'Bacchus Bottle(Glass Bottle)');
+insert into trash_sub_category (ko, en) values ('소주병(유리병)', 'Soju Bottle(Glass Bottle)');
+insert into trash_sub_category (ko, en) values ('음료병(유리병)', 'Drink Bottle(Glass Bottle)');
 
 -- 전용함
-INSERT INTO trash_sub_category (ko, en) VALUES ('일회용컵(페트병류)', 'Disposable Cup(PET Bottles)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('일반페트병(페트병류)', 'General PET Bottle(PET Bottles)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('공통(형광등)', 'Common(Fluorescent Lamp)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('상의(의류)', 'Tops(Clothing)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('원피스(의류)', 'Dress(Clothing)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('하의(의류)', 'Bottoms(Clothing)');
+insert into trash_sub_category (ko, en) values ('일회용컵(페트병류)', 'Disposable Cup(PET Bottles)');
+insert into trash_sub_category (ko, en) values ('일반페트병(페트병류)', 'General PET Bottle(PET Bottles)');
+insert into trash_sub_category (ko, en) values ('공통(형광등)', 'Common(Fluorescent Lamp)');
+insert into trash_sub_category (ko, en) values ('상의(의류)', 'Tops(Clothing)');
+insert into trash_sub_category (ko, en) values ('원피스(의류)', 'Dress(Clothing)');
+insert into trash_sub_category (ko, en) values ('하의(의류)', 'Bottoms(Clothing)');
 
 -- 종량제
-INSERT INTO trash_sub_category (ko, en) VALUES ('도마(나무)', 'Cutting Board(Wood)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('액자(나무)', 'Frame(Wood)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('장식품(나무)', 'Ornament(Wood)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('주걱(나무)', 'Spatula(Wood)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('주방용품(나무)', 'Kitchenware(Wood)');
+insert into trash_sub_category (ko, en) values ('도마(나무)', 'Cutting Board(Wood)');
+insert into trash_sub_category (ko, en) values ('액자(나무)', 'Frame(Wood)');
+insert into trash_sub_category (ko, en) values ('장식품(나무)', 'Ornament(Wood)');
+insert into trash_sub_category (ko, en) values ('주걱(나무)', 'Spatula(Wood)');
+insert into trash_sub_category (ko, en) values ('주방용품(나무)', 'Kitchenware(Wood)');
 
 -- 종이류
-INSERT INTO trash_sub_category (ko, en) VALUES ('노트(종이)', 'Note(Paper)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('상자류(종이)', 'Boxes(Paper)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('신문지(종이)', 'Newspaper(Paper)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('음료수곽(종이)', 'Beverage Carton(Paper)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('포장상자(종이)', 'Packaging Box(Paper)');
+insert into trash_sub_category (ko, en) values ('노트(종이)', 'Note(Paper)');
+insert into trash_sub_category (ko, en) values ('상자류(종이)', 'Boxes(Paper)');
+insert into trash_sub_category (ko, en) values ('신문지(종이)', 'Newspaper(Paper)');
+insert into trash_sub_category (ko, en) values ('음료수곽(종이)', 'Beverage Carton(Paper)');
+insert into trash_sub_category (ko, en) values ('포장상자(종이)', 'Packaging Box(Paper)');
 
 -- 캔류
-INSERT INTO trash_sub_category (ko, en) VALUES ('음료(캔류)', 'Drink(Cans)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('통조림(캔류)', 'Canned Food(Cans)');
+insert into trash_sub_category (ko, en) values ('음료(캔류)', 'Drink(Cans)');
+insert into trash_sub_category (ko, en) values ('통조림(캔류)', 'Canned Food(Cans)');
 
 -- 폐가전제품
-INSERT INTO trash_sub_category (ko, en) VALUES ('TV(전자제품)', 'TV(Electronics)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('가습기(전자제품)', 'Humidifier(Electronics)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('냉장고(전자제품)', 'Refrigerator(Electronics)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('세탁기(전자제품)', 'Washing Machine(Electronics)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('컴퓨터(전자제품)', 'Computer(Electronics)');
+insert into trash_sub_category (ko, en) values ('TV(전자제품)', 'TV(Electronics)');
+insert into trash_sub_category (ko, en) values ('가습기(전자제품)', 'Humidifier(Electronics)');
+insert into trash_sub_category (ko, en) values ('냉장고(전자제품)', 'Refrigerator(Electronics)');
+insert into trash_sub_category (ko, en) values ('세탁기(전자제품)', 'Washing Machine(Electronics)');
+insert into trash_sub_category (ko, en) values ('컴퓨터(전자제품)', 'Computer(Electronics)');
 
 -- 플라스틱류
-INSERT INTO trash_sub_category (ko, en) VALUES ('대용량통(플라스틱)', 'Large Capacity Container(Plastic)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('밀폐용기(플라스틱)', 'Airtight Container(Plastic)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('바구니(플라스틱)', 'Basket(Plastic)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('욕실용품(플라스틱)', 'Bathroom Supplies(Plastic)');
-INSERT INTO trash_sub_category (ko, en) VALUES ('장난감(플라스틱)', 'Toy(Plastic)');
+insert into trash_sub_category (ko, en) values ('대용량통(플라스틱)', 'Large Capacity Container(Plastic)');
+insert into trash_sub_category (ko, en) values ('밀폐용기(플라스틱)', 'Airtight Container(Plastic)');
+insert into trash_sub_category (ko, en) values ('바구니(플라스틱)', 'Basket(Plastic)');
+insert into trash_sub_category (ko, en) values ('욕실용품(플라스틱)', 'Bathroom Supplies(Plastic)');
+insert into trash_sub_category (ko, en) values ('장난감(플라스틱)', 'Toy(Plastic)');
