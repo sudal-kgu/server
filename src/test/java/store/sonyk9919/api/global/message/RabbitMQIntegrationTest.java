470ff0ae25bc0abe521a577d237dfc5972c5b206
한권희 <jana32496@gmail.com>
2026-03-23 16:44:05 +0900

    test: RabbitMQ DLQ 라우팅 테스트
    - saveAndNotify에서 예외 발생 시 DLQ로 이동해 notifyError 호출하는지 확인

--- src/test/java/store/sonyk9919/api/global/message/RabbitMQIntegrationTest.java
+++ src/test/java/store/sonyk9919/api/global/message/RabbitMQIntegrationTest.java
@@ -1,7 +1,9 @@
 package store.sonyk9919.api.global.message;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
 
@@ -22,6 +24,7 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import store.sonyk9919.api.domain.analysis.dto.AnalysisResponseDto;
 import store.sonyk9919.api.domain.analysis.service.AnalysisResultNotifier;
+import store.sonyk9919.api.global.common.dto.ErrorStatus;
 import store.sonyk9919.api.global.config.property.RabbitMQProperty;
 
 @SpringBootTest
@@ -73,19 +76,7 @@ class RabbitMQIntegrationTest {
     @DisplayName("결과 큐에 메시지가 도착 시 saveAndNotify 호출 확인")
     void listeningResultQueue() throws Exception{
         String requestId = "request-002";
-
-        String jsonPayload = """
-                {
-                    "request_id": "%s",
-                    "count": 3,
-                    "detected_items": []
-                }
-                """.formatted(requestId);
-
-        AnalysisResponseDto responseDto = objectMapper.readValue(
-                jsonPayload,
-                AnalysisResponseDto.class
-        );
+        AnalysisResponseDto responseDto = getDto(requestId);
 
         rabbitTemplate.convertAndSend(
                 property.getExchange(),
@@ -95,6 +86,45 @@ class RabbitMQIntegrationTest {
 
         verify(analysisResultNotifier, timeout(3000))
                 .saveAndNotify(argThat(dto -> dto.getRequestId().equals(requestId)));
+    }
+
+    @Test
+    @DisplayName("결과 큐 처리 실패하면 DLQ로 이동해 notifyError 호출")
+    void deadLetterQueue() throws Exception{
+        doThrow(new RuntimeException("fail"))
+                .when(analysisResultNotifier)
+                .saveAndNotify(any());
+
+        String requestId = "request-003";
+        AnalysisResponseDto responseDto = getDto(requestId);
+
+        rabbitTemplate.convertAndSend(
+                property.getExchange(),
+                property.getResultRoutingKey(),
+                responseDto
+        );
+
+        verify(analysisResultNotifier, timeout(3000))
+                .notifyError(
+                        argThat(id -> id.equals(requestId)),
+                        argThat(status -> status == ErrorStatus.INTERNAL_SERVER_ERROR)
+                );
+    }
+
+    private AnalysisResponseDto getDto(String requestId) throws Exception{
+        String jsonPayload =
+            """
+                {
+                    "request_id": "%s",
+                    "count": 0,
+                    "detected_items": []
+                }
+            """.formatted(requestId);
+
+        return objectMapper.readValue(
+                jsonPayload,
+                AnalysisResponseDto.class
+        );
     }
 
     @TestConfiguration
