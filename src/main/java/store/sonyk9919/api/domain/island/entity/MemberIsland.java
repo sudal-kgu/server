88b8ffd374cbbdf8fef8189208c1271cdc8815aa
hyunj24 <hyunjug7@naver.com>
2026-04-09 11:55:03 +0900

    feat: island 도메인 엔티티 추가
    MemberIsland, IslandItemUsage, Item, MemberResources, LevelConfig, ResourceType 엔티티 생성

---
+++ src/main/java/store/sonyk9919/api/domain/island/entity/MemberIsland.java
@@ -0,0 +1,56 @@
+package store.sonyk9919.api.domain.island.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import store.sonyk9919.api.domain.member.entitiy.MemberAccount;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = "member_account_id"))
+public class MemberIsland {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "island_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private int level;
+
+    @Column(nullable = false)
+    private int cumulativeExp;
+
+    @Column(nullable = false)
+    private int recyclingContributionExp;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_account_id", nullable = false)
+    private MemberAccount memberAccount;
+
+    private MemberIsland(String nickname, MemberAccount memberAccount) {
+        this.nickname = nickname;
+        this.level = 1;
+        this.cumulativeExp = 0;
+        this.recyclingContributionExp = 0;
+        this.memberAccount = memberAccount;
+    }
+
+    public static MemberIsland create(String nickname, MemberAccount memberAccount) {
+        return new MemberIsland(nickname, memberAccount);
+    }
+}
---
+++ src/main/java/store/sonyk9919/api/domain/island/entity/ResourceType.java
@@ -0,0 +1,5 @@
+package store.sonyk9919.api.domain.island.entity;
+
+public enum ResourceType {
+    SHELL, GEM, FUEL
+}
