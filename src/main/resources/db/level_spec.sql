bcc349164bd33d94359324d8b842e85e4c1ce9ae
hyunj24 <hyunjug7@naver.com>
2026-04-09 15:14:59 +0900

    feat: items, level_spec 초기 데이터 SQL 스크립트 추가

---
+++ src/main/resources/db/level_spec.sql
@@ -0,0 +1,7 @@
+insert into level_spec (level, required_exp, recycling_contribution_exp_limit, item_contribution_exp_min) values (1, 500, 500, null);
+insert into level_spec (level, required_exp, recycling_contribution_exp_limit, item_contribution_exp_min) values (2, 1500, 1000, null);
+insert into level_spec (level, required_exp, recycling_contribution_exp_limit, item_contribution_exp_min) values (3, 3000, 1000, 500);
+insert into level_spec (level, required_exp, recycling_contribution_exp_limit, item_contribution_exp_min) values (4, 4500, 500, 1000);
+insert into level_spec (level, required_exp, recycling_contribution_exp_limit, item_contribution_exp_min) values (5, 6250, 750, 1000);
+insert into level_spec (level, required_exp, recycling_contribution_exp_limit, item_contribution_exp_min) values (6, 8250, 1000, 1000);
+insert into level_spec (level, required_exp, recycling_contribution_exp_limit, item_contribution_exp_min) values (7, 11250, 1500, 1000);
