8b97f44ed9ae692c731d72d4cefeb8e54ab8a96e
한권희 <jana32496@gmail.com>
2026-04-10 00:41:40 +0900

    feat: building base,spec 데이터 sql 작성

---
+++ src/main/resources/db/building_base.sql
@@ -0,0 +1,8 @@
+INSERT INTO building_base (category, name, model, required_level, cost_shells, cost_gems) VALUES
+('PRODUCTION', '풍력 발전소', 'model_wind_01', 3, 10000, 0),
+('PRODUCTION', '폐기물 분류기', 'model_waste_01', 3, 10000, 0),
+('PRODUCTION', '옷 재활용 공장', 'model_clothes_01', 4, 30000, 10000),
+('PRODUCTION', '플라스틱 재활용 공장', 'model_plastic_01', 5, 100000, 50000),
+
+('PURIFICATION', '재활용 분류기', 'model_recycle_01', 4, 10000, 0),
+('PURIFICATION', '대기 정화 타워', 'model_purity_01', 5, 20000, 0);
