715958d1c7f0668791ea71df6e2d555a8acdcb84
sonyk9919 <sonyk9919@sonyk9919ui-MacBookAir.local>
2026-04-09 12:46:29 +0900

    feat: 사용자 상세 정보 조회

--- src/main/java/store/sonyk9919/api/domain/member/service/MemberRegisterFacade.java
+++ src/main/java/store/sonyk9919/api/domain/member/service/MemberRegisterFacade.java
@@ -3,11 +3,15 @@ package store.sonyk9919.api.domain.member.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import store.sonyk9919.api.domain.member.dto.MemberProfileResponseDto;
 import store.sonyk9919.api.domain.member.entity.MemberAccount;
 import store.sonyk9919.api.domain.member.entity.MemberProfile;
+import store.sonyk9919.api.domain.member.exception.MemberStatus;
+import store.sonyk9919.api.global.common.exception.CustomException;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MemberRegisterFacade {
     private final MemberAccountService memberAccountService;
     private final MemberProfileService memberProfileService;
@@ -17,5 +21,16 @@ public class MemberRegisterFacade {
         MemberAccount account = memberAccountService.getMemberAccount(memberAccountId);
         MemberProfile profile = memberProfileService.createMemberProfile(nickname);
         account.registerMemberProfile(profile);
+    }
+
+    public MemberProfileResponseDto getProfile(Long memberAccountId) {
+        MemberAccount account = memberAccountService.getMemberAccountWithProfile(memberAccountId);
+        MemberProfile profile = account.getProfile();
+
+        if (profile == null) {
+            throw new CustomException(MemberStatus.MEMBER_PROFILE_NOT_FOUND);
+        }
+
+        return MemberProfileResponseDto.from(profile);
     }
 }
