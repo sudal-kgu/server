62d105f9e80c41f5f23bcae265e8aafeed9af4ea
sonyk9919 <sonyk9919@sonyk9919ui-MacBookAir.local>
2026-04-12 01:32:25 +0900

    chore: MemberProfile 제거

--- src/main/java/store/sonyk9919/api/domain/member/repository/MemberAccountRepository.java
+++ src/main/java/store/sonyk9919/api/domain/member/repository/MemberAccountRepository.java
@@ -1,6 +1,5 @@
 package store.sonyk9919.api.domain.member.repository;
 
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import store.sonyk9919.api.domain.auth.entity.OAuthProviderType;
 import store.sonyk9919.api.domain.member.entity.MemberAccount;
@@ -10,7 +9,4 @@ import java.util.Optional;
 public interface MemberAccountRepository extends JpaRepository<MemberAccount, Long> {
 
     Optional<MemberAccount> findByProviderIdAndType(String providerId, OAuthProviderType type);
-
-    @EntityGraph(attributePaths = { "profile" })
-    Optional<MemberAccount> findWithProfileById(Long id);
 }
--- src/main/java/store/sonyk9919/api/domain/member/repository/MemberProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package store.sonyk9919.api.domain.member.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import store.sonyk9919.api.domain.member.entity.MemberProfile;
-
-public interface MemberProfileRepository extends JpaRepository<MemberProfile, Long> {
-}
