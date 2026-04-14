59fb5f3abe065bca2cd42109e9ebe23e59ca6c6d
sonyk9919 <sonyk9919@sonyk9919ui-MacBookAir.local>
2026-04-14 15:11:14 +0900

    test: 퀴즈 선택

--- src/main/java/store/sonyk9919/api/domain/taxonomy/repository/TrashCategoryRepository.java
+++ src/main/java/store/sonyk9919/api/domain/taxonomy/repository/TrashCategoryRepository.java
@@ -3,7 +3,10 @@ package store.sonyk9919.api.domain.taxonomy.repository;
 import org.springframework.data.repository.Repository;
 import store.sonyk9919.api.domain.taxonomy.entity.TrashCategory;
 
+import java.util.Optional;
+
 public interface TrashCategoryRepository extends Repository<TrashCategory, Long> {
+    Optional<TrashCategory> findById(Long id);
     int count();
     boolean existsByKo(String ko);
 }
