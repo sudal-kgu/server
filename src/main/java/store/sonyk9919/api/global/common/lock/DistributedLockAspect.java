7075caef7034538d4590311e2c9a8d4a0f366997
hyunj24 <hyunjug7@naver.com>
2026-04-16 23:41:06 +0900

    feat: Redisson 분산락 인프라 추가

---
+++ src/main/java/store/sonyk9919/api/global/common/lock/DistributedLockAspect.java
@@ -0,0 +1,62 @@
+package store.sonyk9919.api.global.common.lock;
+
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+import org.springframework.stereotype.Component;
+import store.sonyk9919.api.domain.island.exception.IslandStatus;
+import store.sonyk9919.api.global.common.exception.CustomException;
+
+@Aspect
+@Component
+@Order(Ordered.LOWEST_PRECEDENCE - 1)  // @Transactional보다 먼저 실행 → 락이 트랜잭션을 감쌈
+@RequiredArgsConstructor
+public class DistributedLockAspect {
+
+    private final RedissonClient redissonClient;
+    private static final ExpressionParser PARSER = new SpelExpressionParser();
+
+    @Around("@annotation(distributedLock)")
+    public Object around(ProceedingJoinPoint joinPoint, DistributedLock distributedLock) throws Throwable {
+        String lockKey = "lock:" + resolveKey(distributedLock.key(), joinPoint);
+        RLock lock = redissonClient.getLock(lockKey);
+
+        boolean acquired = lock.tryLock(
+                distributedLock.waitTime(),
+                distributedLock.leaseTime(),
+                distributedLock.timeUnit()
+        );
+        if (!acquired) {
+            throw new CustomException(IslandStatus.LOCK_ACQUISITION_FAILED);
+        }
+
+        try {
+            return joinPoint.proceed();
+        } finally {
+            if (lock.isHeldByCurrentThread()) {
+                lock.unlock();
+            }
+        }
+    }
+
+    private String resolveKey(String keyExpression, ProceedingJoinPoint joinPoint) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        String[] paramNames = signature.getParameterNames();
+        Object[] args = joinPoint.getArgs();
+
+        StandardEvaluationContext context = new StandardEvaluationContext();
+        for (int i = 0; i < paramNames.length; i++) {
+            context.setVariable(paramNames[i], args[i]);
+        }
+        return PARSER.parseExpression(keyExpression).getValue(context, String.class);
+    }
+}
--- src/main/java/store/sonyk9919/api/global/config/RedisConfig.java
+++ src/main/java/store/sonyk9919/api/global/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package store.sonyk9919.api.global.config;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -37,6 +40,13 @@ public class RedisConfig {
         redisTemplate.setHashValueSerializer(new StringRedisSerializer());
 
         return redisTemplate;
+    }
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://" + host + ":" + port);
+        return Redisson.create(config);
     }
 
     @Bean
