241da047d7931faf64a86cb61cd7449b41b81bd4
hyunj24 <hyunjug7@naver.com>
2026-04-16 23:50:07 +0900

    feat: 섬 레벨업 이벤트 및 관련 예외 상태 추가

--- src/main/java/store/sonyk9919/api/domain/island/repository/MemberIslandRepository.java
+++ src/main/java/store/sonyk9919/api/domain/island/repository/MemberIslandRepository.java
@@ -1,15 +1,11 @@
 package store.sonyk9919.api.domain.island.repository;
 
-import jakarta.persistence.LockModeType;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import store.sonyk9919.api.domain.island.entity.MemberIsland;
+
+import java.util.Optional;
 
 public interface MemberIslandRepository extends JpaRepository<MemberIsland, Long> {
 
     Optional<MemberIsland> findByMemberAccountId(Long MemberAccountId);
-
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    Optional<MemberIsland> findWithWriteLockByMemberAccountId(Long memberAccountId);
 }
