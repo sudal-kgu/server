33f6b184cc15d3a2707da8e867a72bfd130d3ff2
한권희 <jana32496@gmail.com>
2026-04-16 17:54:22 +0900

    fix: 슬롯 조회시 fetch join으로 건물관련 정보도 한번에 가져오도록 수정

--- src/main/java/store/sonyk9919/api/domain/slot/repository/SlotRepository.java
+++ src/main/java/store/sonyk9919/api/domain/slot/repository/SlotRepository.java
@@ -2,12 +2,17 @@ package store.sonyk9919.api.domain.slot.repository;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import store.sonyk9919.api.domain.island.entity.MemberIsland;
 import store.sonyk9919.api.domain.slot.entity.Slot;
 
 public interface SlotRepository extends JpaRepository<Slot, Long> {
+
+    @EntityGraph(attributePaths = {"building", "building.buildingMetadata"})
     List<Slot> findAllByIsland(MemberIsland island);
+
+    @EntityGraph(attributePaths = {"building", "building.buildingMetadata"})
     Optional<Slot> findByIslandAndSlotNumber(MemberIsland island, Integer slotNumber);
 
     int countByIslandAndActivatedTrue(MemberIsland island);
