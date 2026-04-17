b9c52ed54096715d1654f2f72e016e22e26fc048
sonyk9919 <sonyk9919@sonyk9919ui-MacBookAir.local>
2026-04-17 14:14:48 +0900

    refact: 퀴즈 선택 기능 성능 개선
    - 기존 쿼리 단에서 RAND()를 통해 퀴즈를 랜덤으로 선택함
    - 이는 매 쿼리마다 테이블 풀 스캔을 야기하며, 동시 접속자가 많거나 퀴즈가 많아지는 경우 CPU 또는 메모리 사용량에 문제가 생길 수 있다.
    - 따라서 서비스 레이어에서 무작위성을 부여하도록 개선한다.

--- src/main/java/store/sonyk9919/api/domain/quiz/service/QuizSelectService.java
+++ src/main/java/store/sonyk9919/api/domain/quiz/service/QuizSelectService.java
@@ -1,26 +1,33 @@
 package store.sonyk9919.api.domain.quiz.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import store.sonyk9919.api.domain.quiz.entity.Quiz;
 import store.sonyk9919.api.domain.quiz.exception.QuizStatus;
 import store.sonyk9919.api.domain.quiz.repository.QuizRepository;
 import store.sonyk9919.api.domain.taxonomy.entity.TrashCategory;
 import store.sonyk9919.api.global.common.exception.CustomException;
 
+import java.util.Collections;
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class QuizSelectService {
 
     private final QuizRepository quizRepository;
 
     public List<Quiz> select(TrashCategory category, int number) {
-        List<Quiz> quizzes = quizRepository.findAllByCategory(category.getId(), number);
-        if (quizzes.size() != number) {
+        List<Long> quizzes = quizRepository.findAllIdByCategory(category.getId());
+        if (quizzes.size() < number) {
             throw new CustomException(QuizStatus.NOT_ENOUGH_QUIZ_NUMBER);
         }
-        return quizzes;
+        Collections.shuffle(quizzes);
+        List<Long> ids = quizzes.subList(0, number);
+        return quizRepository.findAllByIdIn(ids);
     }
 }
