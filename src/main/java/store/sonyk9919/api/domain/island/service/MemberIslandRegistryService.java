21ed0703979ddca5f44ad883cd2b5905ab6474bf
한권희 <jana32496@gmail.com>
2026-04-19 21:50:37 +0900

    fix: MemberIslandRegistryService 중복 메서드 삭제
    - getIslandWithWriteLock이 중복되서 작성되어 브런치 충돌

--- src/main/java/store/sonyk9919/api/domain/island/service/MemberIslandRegistryService.java
+++ src/main/java/store/sonyk9919/api/domain/island/service/MemberIslandRegistryService.java
@@ -46,9 +46,4 @@ public class MemberIslandRegistryService {
                 .orElseThrow(() -> new CustomException(IslandStatus.NOT_FOUND_ISLAND));
         return MemberIslandDto.from(island);
     }
-
-    public MemberIsland getIslandWithWriteLock(Long memberAccountId) {
-        return memberIslandRepository.findWithWriteLockByMemberAccountId(memberAccountId)
-                .orElseThrow(() -> new CustomException(IslandStatus.NOT_FOUND_ISLAND));
-    }
 }
