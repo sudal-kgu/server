446cb5ca3f95824be2a1d670d20be8f039c09e89
hyunj24 <hyunjug7@naver.com>
2026-04-24 16:06:08 +0900

    refactor: ResourceChange delta 부호 의존 제거, OperationType으로 연산 명시화

--- src/main/java/store/sonyk9919/api/domain/island/dto/ResourceChange.java
+++ src/main/java/store/sonyk9919/api/domain/island/dto/ResourceChange.java
@@ -3,6 +3,7 @@ package store.sonyk9919.api.domain.island.dto;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import store.sonyk9919.api.domain.island.entity.OperationType;
 import store.sonyk9919.api.domain.island.entity.ResourceType;
 
 @Getter
@@ -11,12 +12,13 @@ public class ResourceChange {
 
     private final ResourceType type;
     private final long delta;
+    private final OperationType operation;
 
     public static ResourceChange add(ResourceType type, long amount) {
-        return new ResourceChange(type, amount);
+        return new ResourceChange(type, amount, OperationType.ADD);
     }
 
     public static ResourceChange subtract(ResourceType type, long amount) {
-        return new ResourceChange(type, -amount);
+        return new ResourceChange(type, amount, OperationType.SUBTRACT);
     }
 }
