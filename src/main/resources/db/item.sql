4a38594be9727db1918d11456d3a700934ca7324
hyunj24 <hyunjug7@naver.com>
2026-05-07 14:15:04 +0900

    fix: 아이템 해금 레벨 및 최대 구매 수량 수정

--- src/main/resources/db/item.sql
+++ src/main/resources/db/item.sql
@@ -1,5 +1,5 @@
 insert into item (name, price, max_count, unlock_level, exp_reward) values ('쓰레기 제거 (소)', 100, 2, 3, 250);
 insert into item (name, price, max_count, unlock_level, exp_reward) values ('쓰레기 제거 (대)', 100, 4, 3, 250);
-insert into item (name, price, max_count, unlock_level, exp_reward) values ('토양 정화', 50, 8, 3, 125);
-insert into item (name, price, max_count, unlock_level, exp_reward) values ('수질 개선', 50, 10, 3, 125);
-insert into item (name, price, max_count, unlock_level, exp_reward) values ('나무 심기', 30, 20, 3, 75);
+insert into item (name, price, max_count, unlock_level, exp_reward) values ('토양 정화', 50, 4, 3, 125);
+insert into item (name, price, max_count, unlock_level, exp_reward) values ('수질 개선', 50, 10, 4, 125);
+insert into item (name, price, max_count, unlock_level, exp_reward) values ('나무 심기', 30, 10, 5, 75);
