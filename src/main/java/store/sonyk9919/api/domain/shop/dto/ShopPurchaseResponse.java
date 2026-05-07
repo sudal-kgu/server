88b5e3b0dcdee25c34e9fc1dd44e993ae62fbb4d
hyunj24 <hyunjug7@naver.com>
2026-05-07 14:21:28 +0900

    feat: 아이템 구매 응답에 레벨업 결과 포함

--- src/main/java/store/sonyk9919/api/domain/shop/dto/ShopPurchaseResponse.java
+++ src/main/java/store/sonyk9919/api/domain/shop/dto/ShopPurchaseResponse.java
@@ -3,6 +3,7 @@ package store.sonyk9919.api.domain.shop.dto;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import store.sonyk9919.api.domain.island.dto.LevelUpResult;
 import store.sonyk9919.api.domain.island.entity.IslandItemUsage;
 import store.sonyk9919.api.domain.island.entity.Item;
 
@@ -15,14 +16,16 @@ public class ShopPurchaseResponse {
     private final long remainingShell;
     private final long currentCount;
     private final int expReward;
+    private final LevelUpResult levelUpResult;
 
-    public static ShopPurchaseResponse of(Item item, IslandItemUsage usage, long remainingShell) {
+    public static ShopPurchaseResponse of(Item item, IslandItemUsage usage, long remainingShell, LevelUpResult levelUpResult) {
         return new ShopPurchaseResponse(
                 item.getId(),
                 item.getName(),
                 remainingShell,
                 usage.getUseCount(),
-                item.getExpReward()
+                item.getExpReward(),
+                levelUpResult
         );
     }
 }
