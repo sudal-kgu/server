8abfabaa90deef45ed56b34e315641fdc598d6f1
hyunj24 <hyunjug7@naver.com>
2026-05-13 00:13:16 +0900

    test: 다음 레벨 도달 조건 응답 테스트 추가

---
+++ src/test/java/store/sonyk9919/api/domain/island/service/MemberIslandRegistryServiceTest.java
@@ -0,0 +1,107 @@
+package store.sonyk9919.api.domain.island.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import store.sonyk9919.api.domain.island.dto.MemberIslandDto;
+import store.sonyk9919.api.domain.island.entity.LevelSpec;
+import store.sonyk9919.api.domain.island.entity.MemberIsland;
+import store.sonyk9919.api.domain.island.repository.MemberIslandRepository;
+import store.sonyk9919.api.domain.member.service.MemberAccountService;
+import store.sonyk9919.api.domain.slot.service.SlotSetupService;
+
+@ExtendWith(MockitoExtension.class)
+class MemberIslandRegistryServiceTest {
+
+    @Mock private MemberIslandRepository memberIslandRepository;
+    @Mock private MemberAccountService memberAccountService;
+    @Mock private SlotSetupService slotSetupService;
+    @Mock private ResourceSetupService resourceSetupService;
+    @Mock private LevelSpecCache levelSpecCache;
+    @InjectMocks private MemberIslandRegistryService memberIslandRegistryService;
+
+    private static final Long MEMBER_ID = 1L;
+    private MemberIsland island;
+    private LevelSpec currentSpec;
+
+    @BeforeEach
+    void setUp() {
+        island = mock(MemberIsland.class);
+        currentSpec = mock(LevelSpec.class);
+        given(memberIslandRepository.findByMemberAccountId(MEMBER_ID)).willReturn(Optional.of(island));
+    }
+
+    @Test
+    void getIslandDto_최고레벨이_아닐_때_nextLevel이_포함된다() {
+        // given
+        given(island.isMaxLevel()).willReturn(false);
+        given(island.getLevel()).willReturn(2);
+        given(levelSpecCache.get(2)).willReturn(currentSpec);
+        given(currentSpec.getRequiredExp()).willReturn(1500);
+        given(currentSpec.getRecyclingContributionExpLimit()).willReturn(1000);
+        given(island.getCumulativeExp()).willReturn(500);
+        given(island.getRecyclingContributionExp()).willReturn(0);
+
+        // when
+        MemberIslandDto dto = memberIslandRegistryService.getIslandDto(MEMBER_ID);
+
+        // then
+        assertThat(dto.getNextLevel()).isNotNull();
+    }
+
+    @Test
+    void getIslandDto_최고레벨일_때_nextLevel이_null이다() {
+        // given
+        given(island.isMaxLevel()).willReturn(true);
+
+        // when
+        MemberIslandDto dto = memberIslandRegistryService.getIslandDto(MEMBER_ID);
+
+        // then
+        assertThat(dto.getNextLevel()).isNull();
+    }
+
+    @Test
+    void getIslandDto_레벨3_진입직후_itemRequired가_true다() {
+        // given
+        given(island.isMaxLevel()).willReturn(false);
+        given(island.getLevel()).willReturn(3);
+        given(levelSpecCache.get(3)).willReturn(currentSpec);
+        given(currentSpec.getRequiredExp()).willReturn(3000);
+        given(currentSpec.getRecyclingContributionExpLimit()).willReturn(1000);
+        given(island.getCumulativeExp()).willReturn(1500);
+        given(island.getRecyclingContributionExp()).willReturn(0);
+
+        // when
+        MemberIslandDto dto = memberIslandRegistryService.getIslandDto(MEMBER_ID);
+
+        // then
+        assertThat(dto.getNextLevel().isItemRequired()).isTrue();
+    }
+
+    @Test
+    void getIslandDto_레벨2_진입직후_itemRequired가_false다() {
+        // given
+        given(island.isMaxLevel()).willReturn(false);
+        given(island.getLevel()).willReturn(2);
+        given(levelSpecCache.get(2)).willReturn(currentSpec);
+        given(currentSpec.getRequiredExp()).willReturn(1500);
+        given(currentSpec.getRecyclingContributionExpLimit()).willReturn(1000);
+        given(island.getCumulativeExp()).willReturn(500);
+        given(island.getRecyclingContributionExp()).willReturn(0);
+
+        // when
+        MemberIslandDto dto = memberIslandRegistryService.getIslandDto(MEMBER_ID);
+
+        // then
+        assertThat(dto.getNextLevel().isItemRequired()).isFalse();
+    }
+}
