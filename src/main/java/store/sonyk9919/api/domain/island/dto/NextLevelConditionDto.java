c0e499957309032682206a93002b25a2fb36fffb
hyunj24 <hyunjug7@naver.com>
2026-05-14 23:30:44 +0900

    refactor: nextLevel 응답에서 itemRequired 제거

--- src/main/java/store/sonyk9919/api/domain/island/dto/NextLevelConditionDto.java
+++ src/main/java/store/sonyk9919/api/domain/island/dto/NextLevelConditionDto.java
@@ -4,22 +4,17 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import store.sonyk9919.api.domain.island.entity.LevelSpec;
-import store.sonyk9919.api.domain.island.entity.MemberIsland;
 
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class NextLevelConditionDto {
     private final int totalRequiredExp;
     private final int recyclingExpLimit;
-    private final boolean itemRequired;
 
-    public static NextLevelConditionDto from(LevelSpec currentSpec, MemberIsland island) {
-        int remainingExp = currentSpec.getRequiredExp() - island.getCumulativeExp();
-        int remainingRecyclingExp = currentSpec.getRecyclingContributionExpLimit() - island.getRecyclingContributionExp();
+    public static NextLevelConditionDto from(LevelSpec currentSpec) {
         return new NextLevelConditionDto(
                 currentSpec.getRequiredExp(),
-                currentSpec.getRecyclingContributionExpLimit(),
-                remainingRecyclingExp < remainingExp
+                currentSpec.getRecyclingContributionExpLimit()
         );
     }
 }
--- src/test/java/store/sonyk9919/api/domain/island/dto/NextLevelConditionDtoTest.java
+++ src/test/java/store/sonyk9919/api/domain/island/dto/NextLevelConditionDtoTest.java
@@ -8,22 +8,21 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import store.sonyk9919.api.domain.island.entity.LevelSpec;
-import store.sonyk9919.api.domain.island.entity.MemberIsland;
 
 @ExtendWith(MockitoExtension.class)
 class NextLevelConditionDtoTest {
 
     @Test
-    void from_레벨1_초기상태_itemRequired가_false다() {
+    void from_totalRequiredExp와_recyclingExpLimit이_currentSpec_값으로_설정된다() {
         // given
-        LevelSpec spec = mockSpec(500, 500);
-        MemberIsland island = mockIsland(0, 0);
+        LevelSpec spec = mockSpec(3000, 1000);
 
         // when
-        NextLevelConditionDto dto = NextLevelConditionDto.from(spec, island);
+        NextLevelConditionDto dto = NextLevelConditionDto.from(spec);
 
         // then
-        assertThat(dto.isItemRequired()).isFalse();
+        assertThat(dto.getTotalRequiredExp()).isEqualTo(3000);
+        assertThat(dto.getRecyclingExpLimit()).isEqualTo(1000);
     }
 
     private LevelSpec mockSpec(int requiredExp, int recyclingExpLimit) {
@@ -31,78 +30,5 @@ class NextLevelConditionDtoTest {
         given(spec.getRequiredExp()).willReturn(requiredExp);
         given(spec.getRecyclingContributionExpLimit()).willReturn(recyclingExpLimit);
         return spec;
-    }
-
-    private MemberIsland mockIsland(int cumulativeExp, int recyclingContributionExp) {
-        MemberIsland island = mock(MemberIsland.class);
-        given(island.getCumulativeExp()).willReturn(cumulativeExp);
-        given(island.getRecyclingContributionExp()).willReturn(recyclingContributionExp);
-        return island;
-    }
-
-    @Test
-    void from_레벨2_진입직후_itemRequired가_false다() {
-        // given
-        LevelSpec spec = mockSpec(1500, 1000);
-        MemberIsland island = mockIsland(500, 0);
-
-        // when
-        NextLevelConditionDto dto = NextLevelConditionDto.from(spec, island);
-
-        // then
-        assertThat(dto.isItemRequired()).isFalse();
-    }
-
-    @Test
-    void from_레벨3_진입직후_itemRequired가_true다() {
-        // given
-        LevelSpec spec = mockSpec(3000, 1000);
-        MemberIsland island = mockIsland(1500, 0);
-
-        // when
-        NextLevelConditionDto dto = NextLevelConditionDto.from(spec, island);
-
-        // then
-        assertThat(dto.isItemRequired()).isTrue();
-    }
-
-    @Test
-    void from_분리배출_한도_소진_시_itemRequired가_true다() {
-        // given
-        LevelSpec spec = mockSpec(3000, 1000);
-        MemberIsland island = mockIsland(2300, 1000);
-
-        // when
-        NextLevelConditionDto dto = NextLevelConditionDto.from(spec, island);
-
-        // then
-        assertThat(dto.isItemRequired()).isTrue();
-    }
-
-    @Test
-    void from_아이템_선구매로_남은exp가_분리배출가능량보다_적으면_itemRequired가_false다() {
-        // given
-        LevelSpec spec = mockSpec(3000, 1000);
-        MemberIsland island = mockIsland(2800, 0);
-
-        // when
-        NextLevelConditionDto dto = NextLevelConditionDto.from(spec, island);
-
-        // then
-        assertThat(dto.isItemRequired()).isFalse();
-    }
-
-    @Test
-    void from_totalRequiredExp와_recyclingExpLimit이_currentSpec_값으로_설정된다() {
-        // given
-        LevelSpec spec = mockSpec(3000, 1000);
-        MemberIsland island = mockIsland(1500, 0);
-
-        // when
-        NextLevelConditionDto dto = NextLevelConditionDto.from(spec, island);
-
-        // then
-        assertThat(dto.getTotalRequiredExp()).isEqualTo(3000);
-        assertThat(dto.getRecyclingExpLimit()).isEqualTo(1000);
     }
 }
