f3bb708ab360c6fa7813c109d25c40191429e262
hyunj24 <hyunjug7@naver.com>
2026-05-19 01:52:34 +0900

    test: ItemUsageService 테스트 추가

---
+++ src/test/java/store/sonyk9919/api/domain/island/service/ItemUsageServiceTest.java
@@ -0,0 +1,173 @@
+package store.sonyk9919.api.domain.island.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+import java.lang.reflect.Constructor;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+import store.sonyk9919.api.domain.island.dto.ItemCatalogDto;
+import store.sonyk9919.api.domain.island.entity.Item;
+import store.sonyk9919.api.domain.island.entity.MemberIsland;
+import store.sonyk9919.api.domain.island.exception.IslandStatus;
+import store.sonyk9919.api.domain.island.repository.IslandItemUsageRepository;
+import store.sonyk9919.api.domain.island.repository.MemberIslandRepository;
+import store.sonyk9919.api.global.common.exception.CustomException;
+
+@ExtendWith(MockitoExtension.class)
+class ItemUsageServiceTest {
+
+    @Mock private MemberIslandRepository memberIslandRepository;
+    @Mock private IslandItemUsageRepository islandItemUsageRepository;
+    @Mock private ItemCache itemCache;
+    @InjectMocks private ItemUsageService itemUsageService;
+
+    private static final Long MEMBER_ID = 1L;
+    private List<Item> allItems;
+    private Item targetItem;
+    private MemberIsland island;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        targetItem = createItem(1L, "쓰레기 제거 (소)", 100, 2, 3, 250);
+        allItems = List.of(
+                targetItem,
+                createItem(2L, "쓰레기 제거 (대)", 100, 4, 3, 250),
+                createItem(3L, "토양 정화",       50,  8, 3, 125),
+                createItem(4L, "수질 개선",       50, 10, 3, 125),
+                createItem(5L, "나무 심기",       30, 20, 3, 75)
+        );
+        island = mock(MemberIsland.class);
+        given(memberIslandRepository.findByMemberAccountId(MEMBER_ID)).willReturn(Optional.of(island));
+    }
+
+    private Item createItem(Long id, String name, int price, int maxCount, int unlockLevel, int expReward)
+            throws Exception {
+        Constructor<Item> ctor = Item.class.getDeclaredConstructor();
+        ctor.setAccessible(true);
+        Item item = ctor.newInstance();
+        ReflectionTestUtils.setField(item, "id", id);
+        ReflectionTestUtils.setField(item, "name", name);
+        ReflectionTestUtils.setField(item, "price", price);
+        ReflectionTestUtils.setField(item, "maxCount", maxCount);
+        ReflectionTestUtils.setField(item, "unlockLevel", unlockLevel);
+        ReflectionTestUtils.setField(item, "expReward", expReward);
+        return item;
+    }
+
+    @Test
+    void getItemUsages_아이템_5개를_반환한다() {
+        // given
+        given(island.getLevel()).willReturn(3);
+        given(itemCache.getAll()).willReturn(allItems);
+        given(islandItemUsageRepository.findItemIdAndUseCountByIsland(island)).willReturn(List.of());
+
+        // when
+        List<ItemCatalogDto> result = itemUsageService.getItemUsages(MEMBER_ID);
+
+        // then
+        assertThat(result).hasSize(5);
+    }
+
+    @Test
+    void getItemUsages_사용_이력_없으면_currentCount가_0이다() {
+        // given
+        given(island.getLevel()).willReturn(3);
+        given(itemCache.getAll()).willReturn(allItems);
+        given(islandItemUsageRepository.findItemIdAndUseCountByIsland(island)).willReturn(List.of());
+
+        // when
+        List<ItemCatalogDto> result = itemUsageService.getItemUsages(MEMBER_ID);
+
+        // then
+        assertThat(result).allMatch(r -> r.getCurrentCount() == 0);
+    }
+
+    @Test
+    void getItemUsages_사용_이력_있으면_currentCount가_반영된다() {
+        // given
+        given(island.getLevel()).willReturn(3);
+        given(itemCache.getAll()).willReturn(allItems);
+        given(islandItemUsageRepository.findItemIdAndUseCountByIsland(island))
+                .willReturn(List.<Object[]>of(usageRow(targetItem.getId(), 1L)));
+
+        // when
+        List<ItemCatalogDto> result = itemUsageService.getItemUsages(MEMBER_ID);
+
+        // then
+        ItemCatalogDto response = result.stream()
+                .filter(r -> r.getItemId().equals(targetItem.getId()))
+                .findFirst().orElseThrow();
+        assertThat(response.getCurrentCount()).isEqualTo(1L);
+    }
+
+    private Object[] usageRow(Long itemId, long useCount) {
+        return new Object[]{itemId, useCount};
+    }
+
+    @Test
+    void getItemUsages_레벨_미달_시_purchasable이_false다() {
+        // given
+        given(island.getLevel()).willReturn(2);
+        given(itemCache.getAll()).willReturn(allItems);
+        given(islandItemUsageRepository.findItemIdAndUseCountByIsland(island)).willReturn(List.of());
+
+        // when
+        List<ItemCatalogDto> result = itemUsageService.getItemUsages(MEMBER_ID);
+
+        // then
+        assertThat(result).allMatch(r -> !r.isPurchasable());
+    }
+
+    @Test
+    void getItemUsages_레벨_충족_시_purchasable이_true다() {
+        // given
+        given(island.getLevel()).willReturn(3);
+        given(itemCache.getAll()).willReturn(allItems);
+        given(islandItemUsageRepository.findItemIdAndUseCountByIsland(island)).willReturn(List.of());
+
+        // when
+        List<ItemCatalogDto> result = itemUsageService.getItemUsages(MEMBER_ID);
+
+        // then
+        assertThat(result).allMatch(ItemCatalogDto::isPurchasable);
+    }
+
+    @Test
+    void getItemUsages_maxCount_도달_시_purchasable이_false다() {
+        // given
+        given(island.getLevel()).willReturn(3);
+        given(itemCache.getAll()).willReturn(allItems);
+        given(islandItemUsageRepository.findItemIdAndUseCountByIsland(island))
+                .willReturn(List.<Object[]>of(usageRow(targetItem.getId(), 2L)));
+
+        // when
+        List<ItemCatalogDto> result = itemUsageService.getItemUsages(MEMBER_ID);
+
+        // then
+        ItemCatalogDto response = result.stream()
+                .filter(r -> r.getItemId().equals(targetItem.getId()))
+                .findFirst().orElseThrow();
+        assertThat(response.isPurchasable()).isFalse();
+    }
+
+    @Test
+    void getItemUsages_섬이_없으면_예외가_발생한다() {
+        // given
+        given(memberIslandRepository.findByMemberAccountId(MEMBER_ID)).willReturn(Optional.empty());
+
+        // when & then
+        assertThatThrownBy(() -> itemUsageService.getItemUsages(MEMBER_ID))
+                .isInstanceOf(CustomException.class)
+                .hasMessageContaining(IslandStatus.NOT_FOUND_ISLAND.getMessage());
+    }
+}
