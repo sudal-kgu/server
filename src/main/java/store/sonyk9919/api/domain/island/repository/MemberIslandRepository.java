8aea0cd9bed9aced4256adfe455adbfe13cd84df
sonyk9919 <sonyk9919@10.nate.com>
2026-05-20 13:05:18 +0900

    fix: 동점자가 존재하는 경우 전체 랭킹과 나의 랭킹의 등수가 다른 문제

--- src/main/java/store/sonyk9919/api/domain/island/repository/MemberIslandRepository.java
+++ src/main/java/store/sonyk9919/api/domain/island/repository/MemberIslandRepository.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import store.sonyk9919.api.domain.island.entity.MemberIsland;
 import store.sonyk9919.api.domain.island.entity.Region;
 
@@ -11,11 +12,13 @@ public interface MemberIslandRepository extends JpaRepository<MemberIsland, Long
 
     Optional<MemberIsland> findByMemberAccountId(Long memberAccountId);
 
-    Page<MemberIsland> findAllByOrderByCumulativeExpDesc(Pageable pageable);
+    Page<MemberIsland> findAllByOrderByCumulativeExpDescIdAsc(Pageable pageable);
 
-    Page<MemberIsland> findAllByRegionOrderByCumulativeExpDesc(Region region, Pageable pageable);
+    Page<MemberIsland> findAllByRegionOrderByCumulativeExpDescIdAsc(Region region, Pageable pageable);
 
-    int countByCumulativeExpGreaterThan(int cumulativeExp);
+    @Query("SELECT COUNT(i) FROM MemberIsland i WHERE i.cumulativeExp > :exp OR (i.cumulativeExp = :exp AND i.id < :id)")
+    int countRankingsBefore(int exp, Long id);
 
-    int countByRegionAndCumulativeExpGreaterThan(Region region, int cumulativeExp);
+    @Query("SELECT COUNT(i) FROM MemberIsland i WHERE i.region = :region AND (i.cumulativeExp > :exp OR (i.cumulativeExp = :exp AND i.id < :id))")
+    int countRegionalRankingsBefore(Region region, int exp, Long id);
 }
