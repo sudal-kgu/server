89c756d31176eb02970c08b1452f552a404ddfea
hyunj24 <hyunjug7@naver.com>
2026-05-21 01:58:22 +0900

    fix: 쓰레기 제거(소), (대) 아이템 수치 조절

--- src/main/resources/db/item.sql
+++ src/main/resources/db/item.sql
@@ -1,5 +1,5 @@
-insert into item (name, price, max_count, unlock_level, exp_reward) values ('쓰레기 제거 (소)', 100, 2, 3, 250);
-insert into item (name, price, max_count, unlock_level, exp_reward) values ('쓰레기 제거 (대)', 100, 4, 3, 250);
+insert into item (name, price, max_count, unlock_level, exp_reward) values ('쓰레기 제거 (소)', 25, 10, 2, 50);
+insert into item (name, price, max_count, unlock_level, exp_reward) values ('쓰레기 제거 (대)', 40, 3, 3, 80);
 insert into item (name, price, max_count, unlock_level, exp_reward) values ('토양 정화', 50, 4, 3, 125);
 insert into item (name, price, max_count, unlock_level, exp_reward) values ('수질 개선', 50, 10, 4, 125);
 insert into item (name, price, max_count, unlock_level, exp_reward) values ('나무 심기', 30, 10, 5, 75);
