2a0f17edd44150ef7709f7329a566ee879697cce
sonyk9919 <sonyk9919@sonyk9919ui-MacBookAir.local>
2026-05-26 16:20:38 +0900

    test: 테스트 실패

--- src/test/java/store/sonyk9919/api/domain/quiz/service/QuizPlayServiceConcurrencyTest.java
+++ src/test/java/store/sonyk9919/api/domain/quiz/service/QuizPlayServiceConcurrencyTest.java
@@ -92,6 +92,7 @@ class QuizPlayServiceConcurrencyTest {
                 Long.class, memberAccountId);
 
         jdbcTemplate.update("INSERT INTO member_resource (resource_type, amount, island_id) VALUES ('SHELL', 0, ?)", islandId);
+        jdbcTemplate.update("INSERT INTO member_resource (resource_type, amount, island_id) VALUES ('GEM', 0, ?)", islandId);
         jdbcTemplate.update("INSERT INTO member_resource (resource_type, amount, island_id) VALUES ('FUEL', 0, ?)", islandId);
 
         itemId = jdbcTemplate.queryForObject("SELECT item_id FROM item LIMIT 1", Long.class);
