4a73ddb2dde01519501bc76ebb8b2f36e4623c93
sonyk9919 <sonyk9919@sonyk9919ui-MacBookAir.local>
2026-05-26 16:08:59 +0900

    feat: 퀴즈 세션 완료 dto 변경

--- src/main/java/store/sonyk9919/api/domain/quiz/dto/RecyclingRewardResponse.java
+++ src/main/java/store/sonyk9919/api/domain/quiz/dto/RecyclingRewardResponse.java
@@ -5,18 +5,30 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import store.sonyk9919.api.domain.island.dto.LevelUpResult;
 import store.sonyk9919.api.domain.island.dto.MemberIslandDto;
+import store.sonyk9919.api.domain.island.dto.ResourceBalanceResponse;
 
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class RecyclingRewardResponse {
 
-    private final int earnedShell;
-    private final int earnedFuel;
-    private final int earnedExp;
+    private final EarnedResource earned;
+    private final ResourceBalanceResponse resource;
     private final MemberIslandDto island;
     private final LevelUpResult.UnlockNotice notice;
 
-    public static RecyclingRewardResponse of(int shell, int fuel, int exp, LevelUpResult levelUpResult) {
-        return new RecyclingRewardResponse(shell, fuel, exp, levelUpResult.getIsland(), levelUpResult.getNotice());
+    public static RecyclingRewardResponse of(int shell, int fuel, int exp, ResourceBalanceResponse resource, LevelUpResult levelUpResult) {
+        return new RecyclingRewardResponse(EarnedResource.of(shell, fuel, exp), resource, levelUpResult.getIsland(), levelUpResult.getNotice());
+    }
+
+    @Getter
+    @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class EarnedResource {
+        private final int shell;
+        private final int fuel;
+        private final int exp;
+
+        public static EarnedResource of(int shell, int fuel, int exp) {
+            return new EarnedResource(shell, fuel, exp);
+        }
     }
 }
