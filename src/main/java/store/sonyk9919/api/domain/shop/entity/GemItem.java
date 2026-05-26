91ff05f290547b3fa89de38a11221c4ba2981d73
hyunj24 <hyunjug7@naver.com>
2026-05-26 21:18:02 +0900

    feat: GemItem, MemberGemExchange 엔티티 및 레포지토리 추가

---
+++ src/main/java/store/sonyk9919/api/domain/shop/entity/GemItem.java
@@ -0,0 +1,35 @@
+package store.sonyk9919.api.domain.shop.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "gem_item")
+public class GemItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "gem_item_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private int gemCost;
+
+    @Column(nullable = false)
+    private int monthlyLimit;
+}
