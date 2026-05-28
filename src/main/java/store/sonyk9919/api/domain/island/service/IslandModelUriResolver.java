9f032c2d6fdb8231fb10928d80124cf38bf1a495
hyunj24 <hyunjug7@naver.com>
2026-05-28 15:23:03 +0900

    feat: 3D 모델 URI 결정을 위한 IslandModelUriResolver 추가

---
+++ src/main/java/store/sonyk9919/api/domain/island/service/IslandModelUriResolver.java
@@ -0,0 +1,35 @@
+package store.sonyk9919.api.domain.island.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import store.sonyk9919.api.domain.island.entity.IslandItemUsage;
+import store.sonyk9919.api.domain.island.entity.MemberIsland;
+import store.sonyk9919.api.domain.island.repository.IslandItemUsageRepository;
+
+@Component
+@RequiredArgsConstructor
+public class IslandModelUriResolver {
+
+    private static final String SOIL_PURIFICATION_ITEM_NAME = "토양 정화";
+
+    private final IslandItemUsageRepository islandItemUsageRepository;
+    private final ItemCache itemCache;
+
+    @Value("${island.model.base-url}")
+    private String modelBaseUrl;
+
+    public String resolve(MemberIsland island) {
+        int modelIndex = Math.min(island.getLevel(), 2) + (int) getSoilPurificationCount(island);
+        return modelBaseUrl + "/island_" + modelIndex + ".glb";
+    }
+
+    private long getSoilPurificationCount(MemberIsland island) {
+        return itemCache.getAll().stream()
+                .filter(item -> SOIL_PURIFICATION_ITEM_NAME.equals(item.getName()))
+                .findFirst()
+                .flatMap(item -> islandItemUsageRepository.findByIslandAndItem(island, item))
+                .map(IslandItemUsage::getUseCount)
+                .orElse(0L);
+    }
+}
