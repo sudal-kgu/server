bf00f30be8aabdd1698d7225ec0d148de665739f
hyunj24 <hyunjug7@naver.com>
2026-06-01 00:06:50 +0900

    fix: shop_item.sql 파일 insert문 수정

--- src/main/resources/db/shop_item.sql
+++ src/main/resources/db/shop_item.sql
@@ -1,5 +1,5 @@
-insert into item (code, name, price, max_count, unlock_level, exp_reward) values ('TRASH_REMOVAL_SMALL', '쓰레기 제거 (소)', 25, 10, 2, 50);
-insert into item (code, name, price, max_count, unlock_level, exp_reward) values ('TRASH_REMOVAL_LARGE', '쓰레기 제거 (대)', 40, 3, 3, 70);
-insert into item (code, name, price, max_count, unlock_level, exp_reward) values ('SOIL_PURIFICATION', '토양 정화', 50, 4, 2, 125);
-insert into item (code, name, price, max_count, unlock_level, exp_reward) values ('WATER_IMPROVEMENT', '수질 개선', 50, 10, 4, 125);
-insert into item (code, name, price, max_count, unlock_level, exp_reward) values ('TREE_PLANTING', '나무 심기', 30, 10, 5, 75);
+insert into shop_item (code, name, price, max_count, unlock_level, exp_reward) values ('TRASH_REMOVAL_SMALL', '쓰레기 제거 (소)', 25, 10, 2, 50);
+insert into shop_item (code, name, price, max_count, unlock_level, exp_reward) values ('TRASH_REMOVAL_LARGE', '쓰레기 제거 (대)', 40, 3, 3, 70);
+insert into shop_item (code, name, price, max_count, unlock_level, exp_reward) values ('SOIL_PURIFICATION', '토양 정화', 50, 4, 2, 125);
+insert into shop_item (code, name, price, max_count, unlock_level, exp_reward) values ('WATER_IMPROVEMENT', '수질 개선', 50, 10, 4, 125);
+insert into shop_item (code, name, price, max_count, unlock_level, exp_reward) values ('TREE_PLANTING', '나무 심기', 30, 10, 5, 75);
