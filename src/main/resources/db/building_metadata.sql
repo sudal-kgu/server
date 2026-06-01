d81acd5ff9e826f72eb0a6304c8d91848bda6060
hyunj24 <hyunjug7@naver.com>
2026-06-01 21:19:18 +0900

    refactor: building_metadata model 필드를 glb 파일명으로 변경

--- src/main/resources/db/building_metadata.sql
+++ src/main/resources/db/building_metadata.sql
@@ -1,8 +1,8 @@
 INSERT INTO building_metadata (category, name, model) VALUES
-('PRODUCTION', '풍력 발전소', 'model_wind_01'),
-('PRODUCTION', '폐기물 분류기', 'model_waste_01'),
-('PRODUCTION', '옷 재활용 공장', 'model_clothes_01'),
-('PRODUCTION', '플라스틱 재활용 공장', 'model_plastic_01'),
+('PRODUCTION', '풍력 발전소', 'production_wind_generator'),
+('PRODUCTION', '폐기물 분류기', 'production_waste_classifier'),
+('PRODUCTION', '옷 재활용 공장', 'production_clothes_recycling'),
+('PRODUCTION', '플라스틱 재활용 공장', 'production_plastic_recycling'),
 
-('PURIFICATION', '재활용 분류기', 'model_recycle_01'),
-('PURIFICATION', '대기 정화 타워', 'model_purity_01');
+('PURIFICATION', '재활용 분류기', 'purification_recycling_classifier'),
+('PURIFICATION', '대기 정화 타워', 'purification_air_purifier');
